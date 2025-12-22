9d40f8b79ee53ee2af71392ac5a69004735dd085
hamza naama <naamahamza11@gmail.com>
2025-12-22 20:03:29 +0100

    [apple

--- linux.sh
+++ linux.sh
@@ -1,2 +1,2 @@
-g++ main.cpp map_parser.cpp renderer.cpp -I${USER_ZDOTDIR}/glew-2.1.0/include -lSDL2 -lSDL2main -lGL -lX11  ${USER_ZDOTDIR}/glew-2.1.0/lib/libGLEW.a #-fsanitize=address
+g++ main.cpp map_parser.cpp renderer.cpp -I${USER}/glew-2.1.0/include -lSDL2 -lSDL2main -lGL -lX11  ${USER}/glew-2.1.0/lib/libGLEW.a #-fsanitize=address
 # ./a.out
